aa0805022410f199f37c86c166b276dfad194e6a
Dirty Old Town <boloks70@gmail.com>
2025-07-07 18:27:09 +0200

    update all

--- mobile.js
+++ mobile.js
@@ -5,13 +5,13 @@ const bars = document.querySelector(".bars");
 const closeButton = document.getElementById("close-button");
 
 bars.addEventListener("click", () => {
-  navigationMobile.style.width = 80 + "%";
-  navigationMobileList.style.transition = "all 1.8s"
+  navigationMobile.style.width = 100 + "%";
+  navigationMobileList.style.transition = "width 1s, opacity 1.2s"
   navigationMobileList.style.opacity = 1;
 });
 
 closeButton.addEventListener("click", () => {
   navigationMobile.style.width = 0;
   navigationMobileList.style.opacity = 0;
-  navigationMobileList.style.transition = "all 0.4s"
+  navigationMobileList.style.transition = "width 1.4s, opacity 0.4s"
 });
