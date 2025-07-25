1f7157cf1711f00699b56c3841c2b42bb67dc047
Dirty Old Town <boloks70@gmail.com>
2025-07-25 09:04:49 +0200

    create and update slideshow

--- slide.js
+++ slide.js
@@ -1,8 +1,21 @@
-// Create image
-let source = [
-  "images/radionica-za-izradu-ključeva.jpg",
-  "images/radionica2.jpg",
-  "images/radionica3.jpg"
-];
+const arrowleft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+const slideshow = document.querySelector(".slideshow");
+
+let position = 0;
+
+arrowRight.addEventListener("click", () => {
+  position -= 56;
+  if (position > 0) position = 0;
+  if (position <= -168) position = 0;
+
+  slideshow.style.left = position + "rem";
+
+});
+
+arrowleft.addEventListener("click", () => {
+  if (position < 0)   position += 56;
+   slideshow.style.left = position + "rem";
+});
 
 
