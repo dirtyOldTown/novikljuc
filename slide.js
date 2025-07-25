8b61a20a00bd04016c9fd3a09b5b786eb5198177
Dirty Old Town <boloks70@gmail.com>
2025-07-25 17:36:01 +0200

    update all

--- slide.js
+++ slide.js
@@ -5,17 +5,17 @@ const slideshow = document.querySelector(".slideshow");
 let position = 0;
 
 arrowRight.addEventListener("click", () => {
-  position -= 56;
+  position -=100;
   if (position > 0) position = 0;
-  if (position <= -168) position = 0;
+  if (position <= -300) position = 0;
 
-  slideshow.style.left = position + "rem";
+  slideshow.style.left = position + "%";
 
 });
 
 arrowleft.addEventListener("click", () => {
-  if (position < 0)   position += 56;
-   slideshow.style.left = position + "rem";
+  if (position < 0)   position += 100;
+   slideshow.style.left = position + "%";
 });
 
 
