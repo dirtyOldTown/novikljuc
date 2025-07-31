3ef84fe63bc31897dd7053cdb70ce2d42dfbd869
Dirty Old Town <boloks70@gmail.com>
2025-07-31 22:56:55 +0200

    update

--- testemonials.js
+++ testemonials.js
@@ -1,3 +1,36 @@
+// Preload images
+  let images = [
+        'images/avatar1.jpg',
+        'images/avatar2.jpg',
+        'images/avatar3.jpg',
+    ];
+
+    // Loads the images one at a time, then calls the callback function when all images
+    // have been loaded
+    function loadImages(images, index, callback) {
+        if (index < images.length) {
+            let img = new Image();
+            img.src = images[index];
+            images[index] = img;
+            images[index].onload = function() {
+                loadImages(images, ++index, callback);
+            };
+        } else {
+            callback(images);
+        }
+    }
+    window.onload = function() {
+        loadImages(images, 0, (images) => {
+            // Your slideshow code goes here. This is just example code
+            // of adding the images to your document once they are all loaded
+            setInterval(updateTestemonials, 10000);
+            images.forEach((item) => {
+               document.querySelector('body').appendChild(item);
+            });
+        });
+    };
+
+
 const testemonials = [
   {
     text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
@@ -49,7 +82,7 @@ function updateTestemonials() {
   }
 }
 
-setInterval(updateTestemonials, 10000);
+
 
   
 
