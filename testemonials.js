0ae965df766c9d5f5e8038c3a1039c2420c77182
Dirty Old Town <boloks70@gmail.com>
2025-08-04 12:32:44 +0200

    test

--- testemonials.js
+++ testemonials.js
@@ -71,12 +71,12 @@ function preloadImages(images, index) {
   }
 }
 
-progressBar.style.animation = "grow 10s linear infinite";
 preloadImages(images, 0);
 
 setTimeout(() => {
+  progressBar.style.animation = "grow 10s linear infinite";
   setInterval(updateTestemonials, 10000);
-});
+}, 500);
 
 
   
